31348475335684f68747aa478f369e91bda72fa7
RadicalGG <C24435482@myTUDublin.ie>
2024-11-18 15:25:16 +0000

    color box

--- the-course/if_statement.gd
+++ the-course/if_statement.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
+
+
+
+
 func _draw() -> void:
 	var r = get_viewport_rect()
-	r.size.x = r.size.x / 2
-	draw_rect(Rect2(r), Color.RED, true, 10, true)
+	var h = r.size.x / 3
+	
+	var p = get_viewport().get_mouse_position()
+	print(p) 
+	if p.x < h:
+		draw_rect(Rect2(0, 0, h, r.size.y), Color.RED, true, 10, true)
+		draw_rect(Rect2(h, 0, h, r.size.y), Color.BLUE, true, 10, true)
+		draw_rect(Rect2(0, h, h, r.size.y), Color.GREEN, true, 10, true)
+		
+	else:
+		draw_rect(Rect2(0, 0, h, r.size.x), Color.BLUE, true, 10, true)
+		draw_rect(Rect2(h, 0, h, r.size.x), Color.RED, true, 10, true)
+		draw_rect(Rect2(0, h, h, r.size.x), Color.YELLOW, true, 10, true)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +28,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	queue_redraw()
+	
 	pass
